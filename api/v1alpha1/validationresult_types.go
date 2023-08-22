57329831a9472fa0082b6f3d6752e50f686e67fd
Tyler Gillson <tyler.gillson@gmail.com>
2023-08-22 16:05:44 -0600

    first commit

---
+++ api/v1alpha1/validationresult_types.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ValidationResultSpec defines the desired state of ValidationResult
+type ValidationResultSpec struct {
+	Plugin string `json:"plugin"`
+}
+
+type ValidationState string
+
+const (
+	ValidationFailed     ValidationState = "Failed"
+	ValidationInProgress ValidationState = "InProgress"
+	ValidationSucceeded  ValidationState = "Succeeded"
+)
+
+// ValidationResultStatus defines the observed state of ValidationResult
+type ValidationResultStatus struct {
+	State ValidationState `json:"state"`
+
+	// +optional
+	// +patchMergeKey=type
+	// +patchStrategy=merge
+	Conditions []ValidationCondition `json:"conditions,omitempty" patchStrategy:"merge" patchMergeKey:"type"`
+}
+
+type ValidationCondition struct {
+	Status             corev1.ConditionStatus `json:"status"`
+	LastTransitionTime metav1.Time            `json:"lastTransitionTime,omitempty"`
+	// Unique, one-word, CamelCase reason for the condition's last transition.
+	Reason string `json:"reason,omitempty"`
+	// Human-readable message indicating details about last transition.
+	Message  string   `json:"message,omitempty"`
+	Failures []string `json:"failures,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ValidationResult is the Schema for the validationresults API
+type ValidationResult struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ValidationResultSpec   `json:"spec,omitempty"`
+	Status ValidationResultStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ValidationResultList contains a list of ValidationResult
+type ValidationResultList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ValidationResult `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ValidationResult{}, &ValidationResultList{})
+}
---
+++ internal/controller/validationresult_controller.go
@@ -0,0 +1,73 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"time"
+
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/go-logr/logr"
+	validationv1alpha1 "github.com/spectrocloud-labs/valid8or/api/v1alpha1"
+)
+
+// ValidationResultReconciler reconciles a ValidationResult object
+type ValidationResultReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+//+kubebuilder:rbac:groups=validation.spectrocloud.labs,resources=validationresults,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=validation.spectrocloud.labs,resources=validationresults/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=validation.spectrocloud.labs,resources=validationresults/finalizers,verbs=update
+
+func (r *ValidationResultReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	r.Log.V(0).Info("Reconciling ValidationResult", "name", req.Name, "namespace", req.Namespace)
+
+	vr := &validationv1alpha1.ValidationResult{}
+	if err := r.Get(ctx, req.NamespacedName, vr); err != nil {
+		// ignore not-found errors, since they can't be fixed by an immediate requeue
+		if apierrs.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		r.Log.Error(err, "failed to fetch ValidationResult")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	r.Log.V(0).Info("Plugin", vr.Spec.Plugin)
+
+	if vr.Status.State == validationv1alpha1.ValidationFailed || vr.Status.State == validationv1alpha1.ValidationSucceeded {
+		r.Log.V(0).Info("Validation complete", "state", vr.Status.State)
+
+		// TODO: send result to a sink
+	}
+
+	r.Log.V(0).Info("Validation in progress. Requeuing in 30s.", "name", req.Name, "namespace", req.Namespace)
+	return ctrl.Result{RequeueAfter: time.Second * 30}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ValidationResultReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&validationv1alpha1.ValidationResult{}).
+		Complete(r)
+}
