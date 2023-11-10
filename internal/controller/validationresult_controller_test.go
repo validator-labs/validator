679fde70999c0e9e53af3c9544db9c596e0b1e68
Tyler Gillson <tyler.gillson@gmail.com>
2023-11-10 15:33:08 -0700

    test: add integration tests for both validator controllers (#96)
    * test: add integration tests for both controllers
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * fix: ensure failed sink state on error
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: avoid VC clashes btwn controller tests
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: setup helm
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: rename test action
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * fix: ensure ValidationResult Status updates operate on fresh VR
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: give ns teardown more time
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: handle EnvTest teardown edge case
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    ---------
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

---
+++ internal/controller/validationresult_controller_test.go
@@ -0,0 +1,118 @@
+package controller
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	kyaml "sigs.k8s.io/yaml"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/spectrocloud-labs/validator/api/v1alpha1"
+	"github.com/spectrocloud-labs/validator/pkg/constants"
+	//+kubebuilder:scaffold:imports
+)
+
+const (
+	validationResultName = "validator-plugin-aws-service-quota"
+)
+
+var (
+	vrServiceQuota = filepath.Join("testdata", "vr-aws-service-quota.yaml")
+)
+
+var _ = Describe("ValidationResult controller", Ordered, func() {
+
+	BeforeEach(func() {
+		// toggle true/false to enable/disable the ValidationResult controller specs
+		if false {
+			Skip("skipping")
+		}
+	})
+
+	vc := &v1alpha1.ValidatorConfig{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      constants.ValidatorConfig,
+			Namespace: validatorNamespace,
+		},
+		Spec: v1alpha1.ValidatorConfigSpec{
+			Sink: &v1alpha1.Sink{
+				Type:       "slack",
+				SecretName: "slack-secret",
+			},
+		},
+	}
+
+	vr := &v1alpha1.ValidationResult{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      validationResultName,
+			Namespace: validatorNamespace,
+		},
+		Spec: v1alpha1.ValidationResultSpec{
+			Plugin:          "AWS",
+			ExpectedResults: 4,
+		},
+	}
+	vrKey := types.NamespacedName{Name: validationResultName, Namespace: validatorNamespace}
+
+	It("Should hash the ValidationResult and update its Status once a ValidationResult is created", func() {
+		By("By creating a new ValidationResult")
+		ctx := context.Background()
+
+		Expect(k8sClient.Create(ctx, vc)).Should(Succeed())
+		Expect(k8sClient.Create(ctx, vr)).Should(Succeed())
+
+		// Mark the ValidationResult as InProgress
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, vrKey, vr); err != nil {
+				return false
+			}
+			vr.Status.State = v1alpha1.ValidationInProgress
+			if err := k8sClient.Status().Update(ctx, vr); err != nil {
+				return false
+			}
+			return true
+		}, timeout, interval).Should(BeTrue(), "failed to update ValidationResult Status")
+
+		// Wait for the ValidationResult's Status to be updated
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, vrKey, vr); err != nil {
+				return false
+			}
+			stateOk := vr.Status.State == v1alpha1.ValidationInProgress
+			sinkStateOk := vr.Status.SinkState == v1alpha1.SinkEmitNone
+			hashOk := vr.ObjectMeta.Annotations[ValidationResultHash] == vr.Hash()
+			return stateOk && sinkStateOk && hashOk
+		}, timeout, interval).Should(BeTrue(), "failed to update ValidationResult Status")
+	})
+
+	It("Should attempt to emit a message to Slack once the ValidationResult is updated", func() {
+		By("By updating the ValidationResult")
+		ctx := context.Background()
+
+		vrBytes, err := os.ReadFile(vrServiceQuota)
+		Expect(err).NotTo(HaveOccurred())
+
+		vrUpdated := &v1alpha1.ValidationResult{}
+		err = kyaml.Unmarshal(vrBytes, vrUpdated)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(k8sClient.Get(ctx, vrKey, vr)).Should(Succeed())
+		vr.Status = vrUpdated.Status
+		Expect(k8sClient.Status().Update(ctx, vr)).Should(Succeed())
+
+		// Wait for the ValidationResult's Status to be updated
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, vrKey, vr); err != nil {
+				return false
+			}
+			// expect the sink to fail, as we've not created the slack secret
+			sinkStateOk := vr.Status.SinkState == v1alpha1.SinkEmitFailed
+			return sinkStateOk
+		}, timeout, interval).Should(BeTrue(), "failed to update ValidationResult Status")
+	})
+})
---
+++ internal/controller/validatorconfig_controller_test.go
@@ -0,0 +1,108 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	appsv1 "k8s.io/api/apps/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	kyaml "sigs.k8s.io/yaml"
+
+	"github.com/spectrocloud-labs/validator/api/v1alpha1"
+	//+kubebuilder:scaffold:imports
+)
+
+const (
+	networkPluginDeploymentName  = "validator-plugin-network-controller-manager"
+	networkPluginDeploymentImage = "quay.io/spectrocloud-labs/validator-plugin-network"
+	networkPluginVersionPre      = "v0.0.4"
+	networkPluginVersionPost     = "v0.0.5"
+)
+
+var (
+	vcNetwork         = filepath.Join("testdata", "vc-network.yaml")
+	expectedImagePre  = fmt.Sprintf("%s:%s", networkPluginDeploymentImage, networkPluginVersionPre)
+	expectedImagePost = fmt.Sprintf("%s:%s", networkPluginDeploymentImage, networkPluginVersionPost)
+)
+
+var _ = Describe("ValidatorConfig controller", Ordered, func() {
+
+	BeforeEach(func() {
+		// toggle true/false to enable/disable the ValidatorConfig controller specs
+		if false {
+			Skip("skipping")
+		}
+	})
+
+	vc := &v1alpha1.ValidatorConfig{}
+	vcKey := types.NamespacedName{Name: "validator-config-test", Namespace: validatorNamespace}
+	networkPluginDeploymentKey := types.NamespacedName{Name: networkPluginDeploymentName, Namespace: validatorNamespace}
+	networkPluginDeployment := &appsv1.Deployment{}
+
+	It("Should deploy validator-plugin-network once a ValidatorConfig is created", func() {
+		By("By creating a new ValidatorConfig")
+		ctx := context.Background()
+
+		vcBytes, err := os.ReadFile(vcNetwork)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = kyaml.Unmarshal(vcBytes, vc)
+		Expect(err).NotTo(HaveOccurred())
+
+		Expect(k8sClient.Create(ctx, vc)).Should(Succeed())
+
+		// Wait for the validator-plugin-network Deployment to be deployed
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, networkPluginDeploymentKey, networkPluginDeployment); err != nil {
+				return false
+			}
+			imageOk := networkPluginDeployment.Spec.Template.Spec.Containers[1].Image == expectedImagePre
+			healthy := networkPluginDeployment.Status.ReadyReplicas == networkPluginDeployment.Status.Replicas
+			return imageOk && healthy
+		}, timeout, interval).Should(BeTrue(), "failed to deploy validator-plugin-network")
+	})
+
+	It("Should upgrade validator-plugin-network once the ValidatorConfig is updated", func() {
+		By("Updating the ValidatorConfig")
+		ctx := context.Background()
+
+		Expect(k8sClient.Get(ctx, vcKey, vc)).Should(Succeed())
+
+		vc.Spec.Plugins[0].Chart.Version = networkPluginVersionPost
+		vc.Spec.Plugins[0].Values = strings.ReplaceAll(
+			vc.Spec.Plugins[0].Values, networkPluginVersionPre, networkPluginVersionPost,
+		)
+
+		Expect(k8sClient.Update(ctx, vc)).Should(Succeed())
+
+		// Wait for the validator-plugin-network Deployment to be upgraded
+		Eventually(func() bool {
+			if err := k8sClient.Get(ctx, networkPluginDeploymentKey, networkPluginDeployment); err != nil {
+				return false
+			}
+			imageOk := networkPluginDeployment.Spec.Template.Spec.Containers[1].Image == expectedImagePost
+			healthy := networkPluginDeployment.Status.ReadyReplicas == networkPluginDeployment.Status.Replicas
+			return imageOk && healthy
+		}, timeout, interval).Should(BeTrue(), "failed to upgrade validator-plugin-network")
+	})
+
+	It("Should uninstall validator-plugin-network once the ValidatorConfig is deleted", func() {
+		By("Deleting the ValidatorConfig")
+		ctx := context.Background()
+
+		Expect(k8sClient.Delete(ctx, vc)).Should(Succeed())
+
+		// Wait for validator-plugin-network Deployment to be deleted
+		Eventually(func() bool {
+			err := k8sClient.Get(ctx, networkPluginDeploymentKey, networkPluginDeployment)
+			return apierrs.IsNotFound(err)
+		}, timeout, interval).Should(BeTrue(), "failed to uninstall validator-plugin-network")
+	})
+})
