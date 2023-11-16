1ead15146156ac278aedb2a77cab0604488fda4f
Tyler Gillson <tyler.gillson@gmail.com>
2023-11-16 10:50:25 -0700

    refactor: accept VR in HandleNewValidationResult for flexibility in plugins (#113)
    * refactor: accept VR in HandleNewValidationResult for flexibility in plugins
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: add coverage
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    * test: add coverage
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>
    
    ---------
    
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- pkg/validationresult/validation_result_test.go
+++ pkg/validationresult/validation_result_test.go
@@ -5,9 +5,13 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	ktypes "k8s.io/apimachinery/pkg/types"
 
 	"github.com/spectrocloud-labs/validator/api/v1alpha1"
+	"github.com/spectrocloud-labs/validator/internal/test"
+	"github.com/spectrocloud-labs/validator/pkg/constants"
 	"github.com/spectrocloud-labs/validator/pkg/types"
 	"github.com/spectrocloud-labs/validator/pkg/util/ptr"
 )
@@ -17,7 +21,8 @@ var err = errors.New("error")
 func res(s corev1.ConditionStatus, state v1alpha1.ValidationState) *types.ValidationResult {
 	return &types.ValidationResult{
 		Condition: &v1alpha1.ValidationCondition{
-			Status: s,
+			Status:         s,
+			ValidationRule: constants.ValidationRulePrefix,
 		},
 		State: ptr.Ptr(state),
 	}
@@ -32,7 +37,8 @@ func vr(cs []corev1.ConditionStatus, state v1alpha1.ValidationState, err error) 
 	}
 	for _, c := range cs {
 		condition := v1alpha1.ValidationCondition{
-			Status: c,
+			Status:         c,
+			ValidationRule: constants.ValidationRulePrefix,
 		}
 		if err != nil {
 			condition.Message = validationErrorMsg
@@ -41,6 +47,78 @@ func vr(cs []corev1.ConditionStatus, state v1alpha1.ValidationState, err error) 
 		vr.Status.Conditions = append(vr.Status.Conditions, condition)
 	}
 	return vr
+}
+
+func TestHandleExistingValidationResult(t *testing.T) {
+	cs := []struct {
+		name string
+		nn   ktypes.NamespacedName
+		res  *v1alpha1.ValidationResult
+	}{
+		{
+			name: "ValidationInProgress",
+			nn:   ktypes.NamespacedName{},
+			res:  vr(nil, v1alpha1.ValidationInProgress, nil),
+		},
+		{
+			name: "ValidationFailed",
+			nn:   ktypes.NamespacedName{},
+			res:  vr([]corev1.ConditionStatus{corev1.ConditionFalse}, v1alpha1.ValidationFailed, nil),
+		},
+		{
+			name: "ValidationSucceeded",
+			nn:   ktypes.NamespacedName{},
+			res:  vr(nil, v1alpha1.ValidationSucceeded, nil),
+		},
+	}
+	for _, c := range cs {
+		t.Log(c.name)
+		HandleExistingValidationResult(c.nn, c.res, logr.Logger{})
+	}
+}
+
+func TestHandleNewValidationResult(t *testing.T) {
+	cs := []struct {
+		name     string
+		client   test.ClientMock
+		res      *v1alpha1.ValidationResult
+		expected error
+	}{
+		{
+			name: "Pass",
+			client: test.ClientMock{
+				SubResourceMock: test.SubResourceMock{},
+			},
+			res:      vr(nil, v1alpha1.ValidationInProgress, nil),
+			expected: nil,
+		},
+		{
+			name: "Fail (create)",
+			client: test.ClientMock{
+				CreateErrors:    []error{errors.New("creation failed")},
+				SubResourceMock: test.SubResourceMock{},
+			},
+			res:      vr([]corev1.ConditionStatus{corev1.ConditionFalse}, v1alpha1.ValidationFailed, nil),
+			expected: errors.New("creation failed"),
+		},
+		{
+			name: "Fail (status update)",
+			client: test.ClientMock{
+				SubResourceMock: test.SubResourceMock{
+					UpdateErrors: []error{errors.New("update failed")},
+				},
+			},
+			res:      vr(nil, v1alpha1.ValidationSucceeded, nil),
+			expected: errors.New("update failed"),
+		},
+	}
+	for _, c := range cs {
+		t.Log(c.name)
+		err := HandleNewValidationResult(c.client, c.res, logr.Logger{})
+		if err != nil && !reflect.DeepEqual(c.expected.Error(), err.Error()) {
+			t.Errorf("expected (%v), got (%v)", c.expected, err)
+		}
+	}
 }
 
 func TestUpdateValidationResult(t *testing.T) {
