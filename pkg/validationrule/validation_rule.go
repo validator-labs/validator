0090c8c45be1430acbedaf6ac3804851de0ba2a8
Matt Welke <matt.welke@spectrocloud.com>
2024-08-22 12:43:27 -0400

    Tweak names and comments.
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- pkg/validationrule/validation_rule.go
+++ pkg/validationrule/validation_rule.go
@@ -1,33 +1,34 @@
-// Package validationrule describes validation rules in the Validator ecosystem.
+// Package validationrule describes validation rules.
 package validationrule
 
 // Interface defines validation rule behavior.
 type Interface interface {
 	// Name returns the name of the rule.
 	Name() string
-	// SetName sets the name of the rule if it is a rule that supports manually specified names.
+	// SetName sets the name of the rule if it is a rule that requires manually specifying its name.
+	// This should be a no-op for rules that automatically generate their name.
 	SetName(string)
-	// AllowsManualName returns whether the validation rule allows manually specifying its name.
-	// Code processing rules should check this first before trying to set a manual name.
-	AllowsManualName() bool
+	// RequiresName returns whether the validation rule requires its name to be manually specified.
+	// This should return false for rules that automatically generate their name.
+	RequiresName() bool
 }
 
-// ManuallyNamed can be embedded into a rule struct to indicate that the rule allows its name to be
-// manually specified.
+// ManuallyNamed can be embedded into a rule struct to indicate that the rule requires its name to
+// be manually specified.
 type ManuallyNamed struct{}
 
-// AllowsManualName returns true, indicating that the rule supports manually specifying its name.
-func (ManuallyNamed) AllowsManualName() bool {
+// RequiresName returns true.
+func (ManuallyNamed) RequiresName() bool {
 	return true
 }
 
-// AutomaticallyNamed can be embedded into a rule struct to indicate that the rule does not allow its
-// name to be manually specified.
+// AutomaticallyNamed can be embedded into a rule struct to indicate that the rule does not require
+// its name to be manually specified because it is automatically generated from other data in the
+// rule.
 type AutomaticallyNamed struct{}
 
-// AllowsManualName returns false, indicating that the rule does not support manually specifying its
-// name.
-func (AutomaticallyNamed) AllowsManualName() bool {
+// RequiresName returns false.
+func (AutomaticallyNamed) RequiresName() bool {
 	return false
 }
 
